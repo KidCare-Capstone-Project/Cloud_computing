164f4a60aff813e96aa0b5a815817dc0635fe21c
Shandy Satria Nugraha <shandysatrian@gmail.com>
2024-11-25 14:15:17 +0700

    Delete src directory

--- src/services/firebaseService.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const admin = require('firebase-admin');
-const serviceAccount = process.env.FIREBASE;
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-module.exports = { admin };
